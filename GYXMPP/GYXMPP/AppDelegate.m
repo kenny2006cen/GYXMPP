aa9d257ce7826ffc3ce175d14ccecdf336d79478
jianglincen <kenny2006cen@163.com>
2016-06-04 10:52:50 +0800

    集成xmppframework

---
+++ GYXMPP/GYXMPP/AppDelegate.m
@@ -0,0 +1,60 @@
+//
+//  AppDelegate.m
+//  GYXMPP
+//
+//  Created by jianglincen on 16/6/4.
+//  Copyright © 2016年 jianglincen. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    application.applicationIconBadgeNumber = 0;
+    self.window.backgroundColor = [UIColor whiteColor];
+    //设置状态栏字体为白色
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault animated:YES];
+    
+    
+    UINavigationController *navLoginVc =[[UINavigationController alloc]initWithRootViewController:[ViewController new]];
+    
+    navLoginVc.title=@"消息主页 ";
+    
+    self.window.rootViewController = navLoginVc;
+
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ GYXMPP/GYXMPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GYXMPP
+//
+//  Created by jianglincen on 16/6/4.
+//  Copyright © 2016年 jianglincen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

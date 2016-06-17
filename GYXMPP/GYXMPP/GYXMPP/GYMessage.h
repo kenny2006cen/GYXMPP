5391828ae48744255b2b28652347beb0a404d3c3
jianglc <jianglc@gyist.com>
2016-06-17 09:32:03 +0800

    修改属性

--- GYXMPP/GYXMPP/GYXMPP/GYMessage.h
+++ GYXMPP/GYXMPP/GYXMPP/GYMessage.h
@@ -14,18 +14,18 @@
 @interface GYMessage : NSObject
 
 // Insert code here to declare functionality of your managed object subclass
-@property (nullable, nonatomic, retain) NSNumber *msgId;
-@property (nullable, nonatomic, retain) NSString *msgUserJid;
-@property (nullable, nonatomic, retain) NSString *msgFriendJid;
-@property (nullable, nonatomic, retain) NSNumber *msgType;
-@property (nullable, nonatomic, retain) NSNumber *msgState;
-@property (nullable, nonatomic, retain) NSNumber *msgRead;
-@property (nullable, nonatomic, retain) NSNumber *msgShow;//消息是否显示
+@property (nullable, nonatomic, strong) NSNumber *msgId;
+@property (nullable, nonatomic, copy) NSString *msgUserJid;
+@property (nullable, nonatomic, copy) NSString *msgFriendJid;
+@property (nullable, nonatomic, strong) NSNumber *msgType;
+@property (nullable, nonatomic, strong) NSNumber *msgState;
+@property (nullable, nonatomic, strong) NSNumber *msgRead;
+@property (nullable, nonatomic, strong) NSNumber *msgShow;//消息是否显示
 
 @property (nullable, nonatomic, retain) NSNumber *msgIsSelf;
-@property (nullable, nonatomic, retain) NSString *msgSendTime;
-@property (nullable, nonatomic, retain) NSString *msgRecTime;
-@property (nullable, nonatomic, retain) NSString *msgBody;//json格式
+@property (nullable, nonatomic, copy) NSString *msgSendTime;
+@property (nullable, nonatomic, copy) NSString *msgRecTime;
+@property (nullable, nonatomic, copy) NSString *msgBody;//json格式
 
 @end
 
